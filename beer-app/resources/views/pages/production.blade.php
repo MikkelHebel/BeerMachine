a7f52b114b0f0cb140cc68ad32d5aaab3d2d1c71
VicMunks <vimun23@student.sdu.dk>
2025-10-30 11:59:56 +0100

    Production page updated

--- beer-app/resources/views/pages/production.blade.php
+++ beer-app/resources/views/pages/production.blade.php
@@ -1,6 +1,29 @@
 <x-app>
     <x-navigation-bar></x-navigation-bar>
     
-    <h1>Beer brewing</h1>
+    <form>
+            <select>
+                <option>Pilsner</option>
+                <option>IPA</option>
+                <option>Stout</option>
+                <option>Ale</option>
+                <option>Alcohol Free</option>
+            </select>
+        </div>
 
+        <div>
+            <label>Speed</label>
+            <input>
+        </div>
+
+        <div>
+            <label>Quantity</label>
+            <input>
+        </div>
+
+        <div>
+            <button class="cursor-pointer">Calibrate</button>
+            <button class="cursor-pointer">Clear</button>
+            <button class="cursor-pointer">Submit</button>
+    </form>
 </x-app>
