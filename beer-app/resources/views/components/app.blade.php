aa80db520aeaf5d4e459e0c1fda1cf82f07d30fd
MikkelHebel <88710599+MikkelHebel@users.noreply.github.com>
2025-10-30 11:27:16 +0100

    Fixed charset

--- beer-app/resources/views/components/app.blade.php
+++ beer-app/resources/views/components/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
     <head>
-        <meta charset="utc-8">
+        <meta charset="utf-8">
         <title>Beer Machine</title>
         @vite('resources/css/app.css')
         <link rel="icon" type="image/x-icon" href="{{ asset('nedenunder-favicon.ico') }}">
