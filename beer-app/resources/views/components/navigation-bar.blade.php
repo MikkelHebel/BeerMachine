9fe4e71fa67bdaaf2f7ed82fe2b02050e427c220
VicMunks <vimun23@student.sdu.dk>
2025-10-30 11:07:25 +0100

    it works

--- beer-app/resources/views/components/navigation-bar.blade.php
+++ beer-app/resources/views/components/navigation-bar.blade.php
@@ -4,12 +4,19 @@
         <img src="{{ asset('images/nedenunder-logo.png') }}" alt="Nedenunder Logo" class="h-8 w-auto">
     </a>
 
-    <div class="flex space-x-6">
+    <div class="flex space-x-18 text-base">
         <a href="{{ route('home') }}">Home</a>
         <a href="{{ route('production') }}">Production</a>
         <a href="{{ route('status') }}">Status</a> 
         <a href="{{ route('statistics') }}">Statistics</a>
         <a href="{{ route('admin') }}">Admin</a>
-        <a href="{{ route('settings') }}">Settings</a>
     </div>
+
+    <div class="flex space-x-5">
+        <a href="{{ route('settings') }}">
+            <img src="{{ asset('images/settings.png') }}" alt="Settings Icon" class="h-8 w-auto">
+        </a>
+        <h3 class="from-red-50">ABORT</h3>
+    </div>
+
 </nav>
