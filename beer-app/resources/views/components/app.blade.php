427ef7d3a626a0caa3590884881db0f9491eb037
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-10-30 11:29:40 +0100

    remove container to let nav stretch

--- beer-app/resources/views/components/app.blade.php
+++ beer-app/resources/views/components/app.blade.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utc-8">
-        <title>Beer Machine</title>
-        @vite('resources/css/app.css')
-        <link rel="icon" type="image/x-icon" href="{{ asset('nedenunder-favicon.ico') }}">
+<head>
+    <meta charset="utc-8">
+    <title>Beer Machine</title>
+    @vite('resources/css/app.css')
+    <link rel="icon" type="image/x-icon" href="{{ asset('nedenunder-favicon.ico') }}">
 
-    </head>
-    
-    <body>
-        <main class="container">
-            {{ $slot }}
-        </main>
-    </body>
+</head>
+
+<body>
+    <main>
+        {{ $slot }}
+    </main>
+</body>
 
 </html>
