6b9c5c6358342db817f01b13d8b5835d3e37be87
Rothausen <rasmusrothausen@gmail.com>
2025-11-07 11:16:10 +0100

    Fix input array formatting in vite.config.js

--- beer-app/vite.config.js
+++ beer-app/vite.config.js
@@ -6,10 +6,11 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 
-                'resources/js/app.js',
-                'resources/css/navigation-bar.css'],
+                'resources/css/app.css',
+                'resources/css/navigation-bar.css',
                 'resources/css/home.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
         tailwindcss(),
