c31dafd3d8f09877b44f4612e97e98e222dc5701
GustavSkou <Gustav.skou.rasmussen@gmail.com>
2025-11-17 17:57:33 +0100

    fetching for batch works

--- beer-app/vite.config.js
+++ beer-app/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
                 'resources/css/home.css',
                 'resources/css/login.css',
                 'resources/js/app.js',
+                'resources/js/homepage.js',
             ],
             refresh: true,
         }),
