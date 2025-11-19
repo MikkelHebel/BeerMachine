6dba036a8c511c037365bc52333ee6f5d72aa504
Tralsted <tralsted@hotmail.dk>
2025-11-19 19:18:19 +0100

    wip: graphbox added.

--- beer-app/resources/views/pages/statistics.blade.php
+++ beer-app/resources/views/pages/statistics.blade.php
@@ -3,48 +3,58 @@
     <x-notification></x-notification>
     <x-navigation-bar></x-navigation-bar>
 
-    <h2>Select a beer type to view data from:</h2>
-        <form action="">
-            <div class="beerTypeRadio">
-                <div class="beerRadioOption">
-                    <label>Pilsner</label>
-                    <input id="pilsnerradio" name="beerselection" type="radio" value="0">
-                </div>
-                <div class="beerRadioOption">
-                    <label>Wheat</label>
-                    <input id="wheatradio" name="beerselection" type="radio" value="1">
-                </div>
-                <div class="beerRadioOption">
-                    <label>IPA</label>
-                    <input id="iparadio" name="beerselection" type="radio" value="2">
-                </div>
-                <div class="beerRadioOption">
-                    <label>Stout</label>
-                    <input id="stoutradio" name="beerselection" type="radio" value="3">
-                </div>
-                <div class="beerRadioOption">
-                    <label>Ale</label>
-                    <input id="aleradio" name="beerselection" type="radio" value="4">
-                </div>
-                <div class="beerRadioOption" >
-                    <label>Alcohol Free</label>
-                    <input id="zeroalcoholradio" name="beerselection" type="radio" value="5" onchange="line_graph(x_axis,y_axis)">
-                </div>
+    <div class="statpage">
+        <div class="leftside">
 
-            </div>
-        </form>
-        <div class="DataPlotsContainer">
-            <h3>Data Plots</h3>
+            <h2>Select a beer type to view data from:</h2>
+
+            <form action="">
+                <div class="beerTypeRadio">
+                    <div class="beerRadioOption">
+                        <label>Pilsner</label>
+                        <input id="pilsnerradio" name="beerselection" type="radio" value="0">
+                    </div>
+                    <div class="beerRadioOption">
+                        <label>Wheat</label>
+                        <input id="wheatradio" name="beerselection" type="radio" value="1">
+                    </div>
+                    <div class="beerRadioOption">
+                        <label>IPA</label>
+                        <input id="iparadio" name="beerselection" type="radio" value="2">
+                    </div>
+                    <div class="beerRadioOption">
+                        <label>Stout</label>
+                        <input id="stoutradio" name="beerselection" type="radio" value="3">
+                    </div>
+                    <div class="beerRadioOption">
+                        <label>Ale</label>
+                        <input id="aleradio" name="beerselection" type="radio" value="4">
+                    </div>
+                    <div class="beerRadioOption" >
+                        <label>Alcohol Free</label>
+                        <input id="zeroalcoholradio" name="beerselection" type="radio" value="5" onchange="line_graph(x_axis,y_axis)">
+                    </div>
+                </div>
+            </form>
+
+            <div class="DataPlotsContainer">
+                <h3>Data Plots</h3>
                 <div class="DataPlots">
                     <button class="DataPlotsbuttons" type="button" onclick="">Failure by speed</button>
                     <button class="DataPlotsbuttons" type="button" onclick="">Ingredients</button>
                 </div>
-        </div>
-        <div id="graphView">
-            <canvas id="myChart"></canvas>
+            </div>
         </div>
 
-
+        <div class="rightside">
+            <div id="graphView">
+                <h3 class="graphTitle">Graph View</h3>
+                <div class="graphBox">
+                    <canvas id="myChart"></canvas>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </x-app>
 
