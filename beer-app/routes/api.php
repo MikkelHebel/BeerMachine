cac8949d6f855c902be8e83c0364d4b8c6e854f2
MikkelHebel <mikkelhebelv2@gmail.com>
2025-12-15 14:37:07 +0100

    Fixed API controller authentication bypass

--- beer-app/routes/api.php
+++ beer-app/routes/api.php
@@ -6,11 +6,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\BatchController;
 
-Route::get('/status/batch', [ApiController::class, 'BatchStatus'])->name('batch.status');
-Route::get('/status/machine', [ApiController::class, 'MachineStatus'])->name('machine.status');
-Route::get('/status/inventory', [ApiController::class, 'InventoryStatus'])->name('inventory.status');
-Route::get('/status/queue', [ApiController::class, 'QueueStatus'])->name('queue.status');
-Route::get('/status/maintenance', [ApiController::class, 'MaintenanceStatus'])->name('maintenance.status');
-Route::post('/command', [ApiController::class, 'SendCommand'])->name('send.command');
+Route::middleware('auth')->controller(PageController::class)->group(function () {
+    Route::get('/status/batch', [ApiController::class, 'BatchStatus'])->name('batch.status');
+    Route::get('/status/machine', [ApiController::class, 'MachineStatus'])->name('machine.status');
+    Route::get('/status/inventory', [ApiController::class, 'InventoryStatus'])->name('inventory.status');
+    Route::get('/status/queue', [ApiController::class, 'QueueStatus'])->name('queue.status');
+    Route::get('/status/maintenance', [ApiController::class, 'MaintenanceStatus'])->name('maintenance.status');
+    Route::post('/command', [ApiController::class, 'SendCommand'])->name('send.command');
 
-Route::get('/expected-failure-rate', [BatchController::class, 'expectedFailureRate']);
+    Route::get('/expected-failure-rate', [BatchController::class, 'expectedFailureRate']);
+});
